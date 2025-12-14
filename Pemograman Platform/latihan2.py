a23a85b111485d4acbf162f8999ff2858a5532fa
Ayu Alfi <ayu425537@gmail.com>
2025-12-14 14:03:00 +0700

    label utama jadi

--- Pemograman Platform/latihan2.py
+++ Pemograman Platform/latihan2.py
@@ -6,7 +6,20 @@ window.geometry("300x200")
 icon = PhotoImage(file='Pemograman Platform/A (3).png')
 window.iconphoto(True,icon)
 window.config(bg="#20244d")
-label = Label(window,
+border=Frame(window,
+             bg='#fafcfc',
+             padx=3,
+             pady=3
+)
+border.pack(pady=30)
+label = Label(border,
               text="Hi Everyone",
+              font=('Arial',20, 'bold'),
+              fg='#fafcfc',
+              bg='#20244d',
+              relief=SOLID,
+              padx=20,
+              pady=20
               )
+label.pack()
 window.mainloop()

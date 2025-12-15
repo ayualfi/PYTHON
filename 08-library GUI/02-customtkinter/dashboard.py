7fc42cc8bc7b1a539ff4288337594d9585464aaf
Ayu Alfi <ayu425537@gmail.com>
2025-12-15 15:54:23 +0700

    nambah dashboard

---
+++ 08-library GUI/02-customtkinter/dashboard.py
@@ -0,0 +1,14 @@
+from customtkinter import CTkFrame, CTkLabel
+
+class DashBoard(CTkFrame):
+    def __init__(self, master):
+        super().__init__(master=master)
+        self.configure(
+            fg_color='black'
+        )
+        self.label_judul = CTkLabel(master,
+                                    text="Dashboard",
+                                    font=("Arial", 12, "bold"),
+                                    fg_color='black',
+                                   )
+        self.label_judul.place(x= 20, y=20)

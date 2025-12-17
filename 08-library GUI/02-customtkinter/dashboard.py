72d06178bc1962b695b6ef5c9bd747f29da1e945
Ayu Alfi <ayu425537@gmail.com>
2025-12-17 14:07:24 +0700

    belummm

--- 08-library GUI/02-customtkinter/dashboard.py
+++ 08-library GUI/02-customtkinter/dashboard.py
@@ -4,11 +4,12 @@ class DashBoard(CTkFrame):
     def __init__(self, master):
         super().__init__(master=master)
         self.configure(
-            fg_color='black'
+            fg_color='#fafcfc'
         )
         self.label_judul = CTkLabel(master,
                                     text="Dashboard",
                                     font=("Arial", 12, "bold"),
-                                    fg_color='black',
+                                    fg_color='#fafcfc',
+                                    text_color='black',
                                    )
         self.label_judul.place(x= 20, y=20)

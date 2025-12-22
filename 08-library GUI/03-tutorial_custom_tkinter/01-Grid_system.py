653ea4b5dcf85a28adbd4a4527ac94b6d73b3bfb
Ayu Alfi <ayu425537@gmail.com>
2025-12-22 13:23:51 +0700

    trying the effect of weight dengan sticky

--- 08-library GUI/03-tutorial_custom_tkinter/01-Grid_system.py
+++ 08-library GUI/03-tutorial_custom_tkinter/01-Grid_system.py
@@ -4,11 +4,21 @@ app = CTk()
 app.title("my app")
 app.geometry("400x150")
 
-button = CTkButton(app,
+button1 = CTkButton(app,
                    text="My Button",
                    )
-button.grid(row=0,
+button1.grid(row=0,
             column=0,
             padx=20,
-            pady=20)
+            pady=20,
+            sticky="ew")
+button2=CTkButton(app,
+                  text="My Button")
+button2.grid(row=0,
+            column=1,
+            padx=20,
+            pady=20,
+            sticky="ew")
+app.grid_columnconfigure(0, weight=1)
+app.grid_columnconfigure(1, weight=9)
 app.mainloop()

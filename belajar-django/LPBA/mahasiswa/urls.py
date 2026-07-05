30d25e1fa26848d353663b6b365be94d19bc5eda
Ayu Alfi <ayu425537@gmail.com>
2026-07-05 15:38:56 +0700

    feat: belajar templating html

--- belajar-django/LPBA/mahasiswa/urls.py
+++ belajar-django/LPBA/mahasiswa/urls.py
@@ -5,20 +5,16 @@ urlpatterns =[
   path('', views.beranda, name='beranda_page'),
   
   path('about/', views.about, name='about_page'),
-  
-  path('news/', views.news, name='news_page'),
-  
-  path('contact/', views.contact, name='contact_page'),
-  
-  path('achievement/', views.achievement, name='achievement_page'),
-  
-  path('education/', views.education, name='education_page'),
 
-  path('family/', views.family, name='family_page'),
-  
-  path('product/', views.product, name='product_page'),
-  
-  path('skill/', views.skill, name='skill_page'),
-  
-  path('others/', views.others, name='others_page'),
+  path('pesanan/', views.pesanan, name='pesanan_page'),
+
+  path('pelanggan/', views.pelanggan, name='pelanggan_page'),
+
+  path('produk/', views.produk, name='produk_page'),
+
+  path('keuangan/', views.keuangan, name='keuangan_page'),
+
+  path('laporan/', views.laporan, name='laporan_page'),
+
+  path('pengaturan/', views.pengaturan, name='pengaturan_page'),
 ]
--- belajar-django/LPBA/mahasiswa/views.py
+++ belajar-django/LPBA/mahasiswa/views.py
@@ -2,24 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 def beranda(request):
-  return render(request, 'mahasiswa/beranda.html')
+  return render(request, 'mahasiswa/beranda.html',{
+    "page_title":"Dashboard",
+    "breadcrumb": "Beranda",
+  })
 def about(request):
-  return render(request, 'mahasiswa/about.html')
-def news(request):
-  return render(request, 'mahasiswa/news.html')
-def contact(request):
-  return render(request, 'mahasiswa/contact.html')
-def achievement(request):
-  return render(request, 'mahasiswa/achievement.html')
-def education(request):
-  return render(request, 'mahasiswa/education.html')
-def family(request):
-  
-  return render(request, 'mahasiswa/family.html')
-def product(request):
-  return render(request, 'mahasiswa/product.html')
-def skill(request):
-  return render(request, 'mahasiswa/skill.html')
-def others(request):
-  return render(request, 'mahasiswa/others.html')
+  return render(request, 'mahasiswa/about.html',{
+    "page_title":"about",
+    "breadcrumb": "about",
+  })
+def pesanan(request):
+  return render(request, 'mahasiswa/pesanan.html',{
+    "page_title":"pesanan",
+    "breadcrumb": "pesanan",
+  })
+def pelanggan(request):
+  return render(request, 'mahasiswa/pelanggan.html',{
+    "page_title":"pelanggan",
+    "breadcrumb": "pelanggan",
+  })
+def produk(request):
+  return render(request, 'mahasiswa/produk.html',{
+    "page_title":"produk",
+    "breadcrumb": "produk",
+  })
+def keuangan(request):
+  return render(request, 'mahasiswa/keuangan.html',{
+    "page_title":"keuangan",
+    "breadcrumb": "keuangan",
+  })
+def laporan(request):
+  return render(request, 'mahasiswa/laporan.html',{
+    "page_title":"laporan",
+    "breadcrumb": "laporan",
+  })
+def pengaturan(request):
+  return render(request, 'mahasiswa/pengaturan.html',{
+    "page_title":"pengaturan",
+    "breadcrumb": "pengaturan",
+  })
 
